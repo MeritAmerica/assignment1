4bd43bec08fcb8436fa5a0198abe2d9d445570f9
matthew <mematthew123@gmail.com>
2021-02-27 17:37:30 -0700

    added checking/savings info

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -2,5 +2,64 @@ package com.meritamerica.assignment1;
 
 public class CheckingAccount extends AccountHolder 
 {
+	/*INSTANCE VARIABLES*/
+	private double openingBalance;
+	private int years;
+	
+	
+	
+	private double interestRate =.0001;
+	
+	
 
+	
+	
+	/* Getters*/
+	 private double getBalance() {
+		 
+		 
+		 return openingBalance;
+	 }
+	 
+	 
+	 private double getInterestRate() {
+		 
+		 return interestRate;
+	 }
+	 
+	 
+	 boolean withdraw(double amount) {
+		 
+		 
+		 return 
+		 
+		 
+		 
+	 }
+ boolean deposit(double amount) {
+		 
+		 
+		 return 
+		 
+		 
+		 
+	 }
+	 
+ 
+ double futureValue(int years) {
+	 
+	 
+	 return 
+	 
+	 
+	 
+ }
+	
+ public String toString()
+	{
+		return "\nChecking Account Balance: " + getBalance() +
+				"\nChecking Account Interest Rate: " + getInterestRate() +
+				"\nChecking Account Balance in 3 Years" + futureValue(years);  
+			
+	}
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -2,5 +2,64 @@ package com.meritamerica.assignment1;
 
 public class SavingsAccount extends AccountHolder 
 {
+	/*INSTANCE VARIABLES*/
+	private double openingBalance;
+	private int years;
+
 	
+	
+	private double interestRate =.01;
+	
+	
+
+	
+	
+	/* Getters*/
+	 private double getBalance() {
+		 
+		 
+		 return openingBalance;
+	 }
+	 
+	 
+	 private double getInterestRate() {
+		 
+		 return interestRate;
+	 }
+	 
+	 
+	 boolean withdraw(double amount) {
+		 
+		 
+		 return 
+		 
+		 
+		 
+	 }
+ boolean deposit(double amount) {
+		 
+		 
+		 return 
+		 
+		 
+		 
+	 }
+	 
+ 
+ double futureValue(int years) {
+	 
+	 
+	 return 
+	 
+	 
+	 
+ }
+	
+ public String toString()
+	{
+		return "\nSavings Account Balance: " + getBalance() +
+				"\nSavings Account Interest Rate: " + getInterestRate() +
+				"\nSavings Account Balance in 3 Years" + futureValue(years);  
+			
+	}
 }
