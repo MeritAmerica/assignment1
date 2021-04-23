92feb4d15aafb666dc291c44e77b65486d041767
BigAlEE <achughesee@gmail.com>
2021-04-23 16:28:50 -0400

    src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -2,4 +2,52 @@ package com.meritamerica.assignment1;
 
 public class AccountHolder {
 	
+	private void AccountHolder() {
+		
+	}
+	private void AccountHolder(String firstName, String middleName, String lastName, String ssn,
+			double checkingAccountOpeningBalance, double savingsAccountOpeningBalance) {
+		
+		
+		
+	}
+	
+	public String getFirstName() {
+		
+		
+	}
+	public void setFirstName(String firstName) {
+		
+	}
+	public String getMiddleName() {
+		
+	}
+	
+	public void setMiddleName(String middleName) {
+		
+	}
+	
+	public String getLastName() {
+		
+	}
+	
+	public void setLastName(String lastName) {
+		
+	}
+	
+	public String getSSN() {
+		
+	}
+	
+	void setSSN(String ssn) {
+		
+	}
+	
+	public CheckingAccount getCheckingAccount;{
+		
+	}
+	
+	public SavingsAccount getSavingsAccount;{
+		
+	}
 }
