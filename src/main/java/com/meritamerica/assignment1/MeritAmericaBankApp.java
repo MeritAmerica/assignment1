711c61606c524901e6713e1793730ac11a082ea1
Penprapa Calhoun <mayyeewa@yahoo.com>
2021-04-25 15:04:03 -0400

    first commit

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,19 @@
 package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
+	public static void main(String[] args) {
+		AccountHolder May = new AccountHolder("May", " ","Calhoun", "123456789", 100.0, 1000.0) {
+			System.out.println(May + " " + toString());
+		
+		
+		
+	}
+	
+	
+}
+	
+	
+	
+	
 	
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -1,5 +1,68 @@
 package com.meritamerica.assignment1;
 
 public class SavingsAccount {
+	//Variable
+	private double balance;
+	private double interestRate = 0.01;
+	private double futureValue;
+
+//Constructor
+	public SavingsAccount() {
+		
+	}
+
+	//Special Constructor
+
+public SavingsAccount(double balance) {
+	super();
+	this.balance = balance;
+}
+
+public double getBalance() {
+	return balance;
+}
+
+public double getInterestRate() {
+	return interestRate;
+}
+	
+	public boolean withdraw(double amount) {
+		if ( amount<=balance && !(amount<0)) {
+			balance-=amount;
+			return true;
+			
+			
+		}else
+			return false;
+	}
+	
+	public boolean deposit(double amount) {
+		if (amount>0) {
+		balance+=amount;
+		return true;
+		
+	}else
+		return false;
+
+	
+	
+	}
+	
+	public double futureValue(int years) {
+		double interest=1.01;
+		return Math.pow(interest, years)*this.balance;
+		
+		
+				
+	}
+
+	@Override
+	public String toString() {
+		return "SavingsAccount [balance=" + balance + ", interestRate=" + interestRate + ", futureValue=" + futureValue
+				+ "]";
+	}
+	
+	
 	
 }
+
